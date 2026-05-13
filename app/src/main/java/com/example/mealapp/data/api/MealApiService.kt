461fed489df92a956a9b3db882402ba387aaa1eb
cdcepeda01 <cdcepeda@uniboyaca.edu.co>
2026-05-13 16:59:05 -0500

    Eliminar todos los archivos

--- app/src/main/java/com/example/mealapp/data/api/MealApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mealapp.data.api
-
-import com.example.mealapp.data.model.MealResponse
-import retrofit2.http.GET
-
-interface MealApiService {
-    @GET("random.php")
-    suspend fun getRandomMeal(): MealResponse
-}
